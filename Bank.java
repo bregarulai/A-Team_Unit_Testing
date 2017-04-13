c134c81e742b8d1fa884d5f11dc4036d9a8d8f07
Sharon Tender <sharon.tender@icloud.com>
2017-04-13 15:07:53 -0400

    Bank has main() in it.
    I needed to create an Account.java and Customer.java to eliminate errors in main.

---
+++ Bank.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankofateam;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sharon
+ */
+public class Bank {
+    Bank() {        
+    }
+    
+    void addAccount() {
+        // Creates a new bank account
+    }
+    
+    void removeAccount(String accountId){
+        // Deletes a given account.
+    }
+    
+    void getAccounts() {
+        // Generates a report of all current accounts, in account ID order
+    }
+    
+    Account getAccount(String accountId) {
+        // Flavio
+        // Find an account given an account ID
+        Account a = new Account();
+        return a;
+    }
+    
+    void addCustomer() {
+        // adds a new customer to the bank
+    }
+    
+    void removeCustomer(String customerId) {
+        // Deletes a customer from the bank.
+    }
+    
+    void getCustomers() {
+        // Generates a report of all current customers in customer ID order
+    }
+    
+    Customer getCustomer (String customerId) {
+        // Flavio
+        // Get a Customer object, given a customer's Id
+        Customer c = new Customer();
+        return c;
+    }
+    
+    List<Customer> getCustomer(String lastName, String firsName) {
+        // Flavio
+        // Get a list of Customer objects, given a customer's last and first names.
+        // Assume customer names are unique.
+        List<Customer> c = new ArrayList<>();
+        return List<customer> c;
+    }    
+    
+    List<Account> getCustomersAccounts (String customerId) {
+        // Flavio
+        // Return a list of a given customer's accounts (if any)
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Bank bank = new Bank();
+        
+    }
+}
+
+
+
+
+
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package bankofateam;
+//
+////import .bankutil;
+//
+///**
+// *
+// * @author Lucien & Sharon
+// */
+//public class BankOfATeam {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//        
+//        
+//        Bank bank = new Bank();
+//        
+//    }
+//
+//
+//    
+//}
---
+++ Customer.java
@@ -0,0 +1,16 @@
+/*
+ * Flavio
+ * Temporary file so my file does not show errors.
+ */
+package bankofateam;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    
+    
+
+}
