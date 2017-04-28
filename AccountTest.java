e8817be629697d69175c4ce2f02a1296d14e1a24
Sharon Tender <sharon.tender@icloud.com>
2017-04-28 18:28:11 -0400

    added testing files: Account, Bank, SavingsAccount

---
+++ AccountTest.java
@@ -0,0 +1,105 @@
+/** @author Flavio Aquino, Sharon Tender, Frank Castillo,
+ *  April 2017
+ * 
+ *  Class responsible for testing Account.java
+ */
+package banking;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sharon    // add your name here if you worked on it too!
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+
+    /**
+     * Test of deposit method, of class Account.
+     */
+    @Test
+    public void testDeposit() {
+        System.out.println("deposit");
+        final String lastName = "Doe";
+        final String firstName = "Jane";
+        double amount = 0.00;
+        Bank bank = new Bank("Test Bank");
+        Customer customer = new Customer(bank, lastName, firstName);
+        Account account = new SavingsAccount(customer, amount, "description");
+        account.deposit(amount);                          // don't know where to go from here                            
+        double expResult = 0.00;
+        assertTrue(expResult == amount);
+        assertFalse("Deposit does not equal 0: ", amount != 0.00);
+    }
+
+    /**
+     * Test of withdraw method, of class Account.
+     */
+    @Test
+    public void testWithdraw() {
+        System.out.println("withdraw");
+
+    }
+
+    /**
+     * Test of transfer method, of class Account.
+     */
+    @Test
+    public void testTransfer() {
+        System.out.println("transfer");
+        System.out.println("deposit");
+        final String lastName = "Doe";
+        final String firstName = "Jane";
+        final String lastName2 = "Dont";
+        final String firstName2 = "John";
+        double amount = 0.00;
+        Bank bank = new Bank("Test Bank");
+        Customer fromCustomer = new Customer(bank, lastName, firstName);
+        Customer toCustomer = new Customer(bank, lastName2, firstName2);
+        Account fromAccount = new SavingsAccount(fromCustomer, amount, "description");
+        Account toAccount = new SavingsAccount(toCustomer, amount, "descriptin");
+        banking.Account.transfer(fromAccount, toAccount, amount);
+        assertTrue(fromAccount != toAccount);
+    }
+
+    /**
+     * Test of hashCode method, of class Account.
+     */
+    @Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+
+    }
+
+    /**
+     * Test of equals method, of class Account.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+
+    }
+
+    /**
+     * Test of compareTo method, of class Account.
+     */
+    @Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+
+    }
+
+    /**
+     * Test of toString method, of class Account.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+
+    }
+
+    
+}
---
+++ BankTest.java
@@ -0,0 +1,72 @@
+/** @author Flavio Aquino, Sharon Tender, Frank Castillo,
+ *  April 2017
+ * 
+ *  Class responsible for testing Bank.java
+ */
+package banking;
+
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sharon    // add your name here if you worked on it too!
+ */
+public class BankTest {
+
+    @Test
+    public void testCreateABank () {
+        System.out.println("testCreateABank");
+        final String name = "Foo";
+        Bank theBank = new Bank(name);
+        assertNotNull( theBank );
+        System.out.println(name);                    // prints foo
+        System.out.println(theBank.getNAME());       // prints null
+        assertEquals( name, theBank.getNAME() );     // this is false 
+    }
+       
+    @Test
+    public void testRemoveCustomer() {
+        System.out.println("RemoveCustomer");
+        final String lastName1 = "Doe";
+        final String firstName1 = "Jane";
+        final String lastName2 = "Jones";
+        final String firstName2 = "Amy";
+        final Bank instance = new Bank ("Test-Bank");
+        final String CUSTOMER_1_ID = instance.addCustomer( lastName1, firstName1 );
+        final String CUSTOMER_2_ID = instance.addCustomer( lastName2, firstName2 );
+        assertNotEquals(CUSTOMER_1_ID, CUSTOMER_2_ID);                           // both are null
+        List<Customer> custList = instance.getCustomer( lastName1, firstName1 ); // NullPointerException
+        assertEquals( 2, custList.size() );                                      // checkiing to see if list has 2 items
+        instance.removeCustomer(CUSTOMER_2_ID);                       // remove a customer
+        assertEquals(1, custList.size());                                        // checking to see if removeCustomer worked
+        
+    }
+    
+    @Test
+    public void testAddCustomer() {
+        System.out.println("addCustomer");
+        final String lastName = "Doe";
+        final String firstName = "Jane";
+        final Bank bank = new Bank ("Test-Bank");
+        final String result = bank.addCustomer( lastName, firstName );     // checking to see if addCustomer worked
+        Customer customer = new Customer(bank, lastName, firstName);
+//        List<Customer> custList = new List<>(customer); 
+//        String expResult = lastName + firstName;
+//        assertEquals(1, custList.size());                                        // checking to see if it added a customer        
+//        assertEquals(expResult, result);                                         // result is null                         
+
+        // test for null
+        String customerId = null;  
+        String result2 = customerId;
+        String expResult2 = null;
+        assertEquals(expResult2, result2);
+        assertNull("should be null: ", result2);
+        
+        
+
+    }
+
+
+}
