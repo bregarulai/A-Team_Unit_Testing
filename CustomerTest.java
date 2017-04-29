a4a04efa056f64ffd3af0317b4dc1346d364fa62
Flavio Aquino <bregarulai@gmail.com>
2017-04-28 23:30:52 -0400

    added getter for customer on account

--- CustomerTest.java
+++ CustomerTest.java
@@ -47,7 +47,16 @@ public class CustomerTest {
 		assertNotNull(accounts);
 		
 		fail("Not yet implemented");
-		
+				
+	}
+	
+	@Test
+	public void testYtdFees() {
+		Bank bank = new Bank("New Bank");
+		Customer cust = new Customer(bank, "John", "Doe");
+		Account account = new SavingsAccount(cust, 123.34, "Savings");
+		double result = 123.34 * 2.0;
+		assertEquals(result, account.get);
 		
 	}
 }
