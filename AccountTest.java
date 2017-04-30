95a49a180067d4ba81639c225c5d27bf25b385d1
Flavio Aquino <bregarulai@gmail.com>
2017-04-30 13:24:09 -0400

    Added testTranfer test to AccountTest class

---
+++ AccountTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Test
+	public void testTranfer() {
+		Bank bank = new Bank("New Bank");
+		Customer cust = new Customer(bank, "John", "Doe");
+		Account account1 = new SavingsAccount(cust, 100.00, "Savings");
+		Account account2 = new SavingsAccount(cust, 50.00, "Checking");
+		final double result = 150.00;
+		final double DELTA = 1E-5;
+		Account.transfer(account2, account1, 50.00);
+		assertEquals(result, account1.getBalance(), DELTA);
+		
+		fail("Not yet implemented");
+	}
+
+}
